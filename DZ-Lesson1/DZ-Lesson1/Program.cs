37c50daa901c41cf99d3a062f04ea5b696473475
alex0572 <al0572@yandex.ru>
2019-09-21 22:47:48 +0300

    Второе ДЗ. Программа рассчета и вывода индекс массы тела (ИМТ)

--- DZ-Lesson1/DZ-Lesson1/Program.cs
+++ DZ-Lesson1/DZ-Lesson1/Program.cs
@@ -18,24 +18,44 @@ namespace DZ_Lesson1
                   в) используя вывод со знаком $. Ляшенко А.В. */
 
 
-            Console.Write("Введите имя: ");
-            string name = Console.ReadLine();
+            //Console.Write("Введите имя: ");
+            //string name = Console.ReadLine();
 
-            Console.Write("Введите фамилию: ");
-            string famil = Console.ReadLine();
+            //Console.Write("Введите фамилию: ");
+            //string famil = Console.ReadLine();
 
-            Console.Write("Введите возраст: ");
-            string age = Console.ReadLine();
+            //Console.Write("Введите возраст: ");
+            //string age = Console.ReadLine();
 
-            Console.Write("Введите рост: ");
-            string gain = Console.ReadLine();
+            //Console.Write("Введите рост: ");
+            //string gain = Console.ReadLine();
 
-            Console.Write("Введите вес: ");
-            string weight = Console.ReadLine();
+            //Console.Write("Введите вес: ");
+            //string weight = Console.ReadLine();
 
-            Console.WriteLine("Вас зовут: " + name + " " + famil + ", возраст: " + age + ", рост: " + gain + ", вес: " + weight + "!"); //используя склеивание
-            Console.WriteLine("Вас зовут: {0} {1}, возраст: {2}, рост: {3}, вес: {4}!", name, famil, age, gain, weight); //используя форматированный вывод
-            Console.WriteLine($"Вас зовут: {name} {famil}, возраст: {age}, рост: {gain}, вес: {weight}!"); //используя вывод со знаком $
+            //Console.WriteLine("Вас зовут: " + name + " " + famil + ", возраст: " + age + ", рост: " + gain + ", вес: " + weight + "!"); //используя склеивание
+            //Console.WriteLine("Вас зовут: {0} {1}, возраст: {2}, рост: {3}, вес: {4}!", name, famil, age, gain, weight); //используя форматированный вывод
+            //Console.WriteLine($"Вас зовут: {name} {famil}, возраст: {age}, рост: {gain}, вес: {weight}!"); //используя вывод со знаком $
+
+            //Console.ReadLine();
+
+            #endregion
+
+            #region ДЗ 1-2 Индекс массы тела (ИМТ)  
+
+            /* 	Ввести вес и рост человека. Рассчитать и вывести индекс массы тела (ИМТ) 
+                по формуле I=m/(h*h); где m — масса тела в килограммах, h — рост в метрах. Ляшенко А.В.*/
+
+            Console.Write("Введите вес(кг): ");
+            Double m = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите рост(м): ");
+            Double h = Convert.ToDouble(Console.ReadLine());
+
+            Double I = m / (h * h);
+
+
+            Console.WriteLine($"Индекс массы тела (ИМТ) равен: {I:f2} !"); //используя вывод со знаком $ и спецификатор формата .f2
 
             Console.ReadLine();
 
